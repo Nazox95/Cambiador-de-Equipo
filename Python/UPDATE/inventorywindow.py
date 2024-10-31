d91eb29400882da516c980fc0196cfa6869761d8
Nazox <dani6359@hotmail.es>
2024-10-31 16:08:46 +0100

    Create inventorywindow.py
    -You need inventorywindow.py but you have 2, one locale/xx and 2 in uiscript so, you need know what you use.

---
+++ Python/UPDATE/inventorywindow.py
@@ -0,0 +1,17 @@
+#You need add this in inventorywindow.py but maybe you use in uiscript or locale but
+#is the same, so add in the end
+                        {
+                            ##Cambio de equipo
+                            "name" : "Equipo",
+                            "type" : "button",
+
+                            "x" : X,
+                            "y" : 8+34*2,
+
+                            "tooltip_text" : "Cambio de Equipo",
+
+                            "default_image" : "locale/es/botones/equipo_1.tga",
+                            "over_image" : "locale/es/botones/equipo_2.tga",
+                            "down_image" : "locale/es/botones/equipo_3.tga",
+                        },
+#Don't forget change x and y and locale/xx or your buttons.
